dd54394a47d0a46da534486b3c61a7d46a360064
Victor Brambila <victorbrambila22@gmail.com>
2022-05-24 22:04:05 -0700

    menu slideout fixed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { ReactComponent as MenuLogo } from '../../images/menu-logo.svg';
@@ -8,15 +8,25 @@ const useStyles = makeStyles(theme => {
   return ({
     header: {
       backgroundColor: '#C4C4C4',
-      minHeight: '50px',
       position: 'relative',
       width: '100vw',
-      left: props => props.isMenuOpen ? 350 : 0,
-      transition: '350ms',
+      //left: props => props.isMenuOpen ? 350 : 0,
+      //transition: '350ms',
       boxShadow: '0px 2px 4px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
       height: 70,
       zIndex: '100'
     },
+    headerActive: {
+        left: 350,
+        backgroundColor: '#C4C4C4',
+        position: 'relative',
+        width: '100vw',
+        //left: props => props.isMenuOpen ? 350 : 0,
+        //transition: '550ms',
+        boxShadow: '0px 2px 4px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
+        height: 70,
+        zIndex: '100'
+    }
   });
 });
 
@@ -25,19 +35,17 @@ function Header(props) {
     const classes = useStyles(props);
 
     return (
-    <section className={classes.header}>
-        <div>
-            <Button onClick={() => toggleMenu(isMenuOpen)}>
+        <div className={ isMenuOpen ? classes.headerActive : classes.header }>
+            <Button onClick={() => toggleMenu()}>
                 <MenuLogo /> 
             </Button>
         </div>
-    </section>
     );
 }
 
-Header.propTypes = {
-  toggleMenu: PropTypes.func,
-  isMenuOpen: PropTypes.bool,
-};
+// Header.propTypes = {
+//   toggleMenu: PropTypes.func,
+//   isMenuOpen: PropTypes.bool,
+// };
 
 export default (Header);
