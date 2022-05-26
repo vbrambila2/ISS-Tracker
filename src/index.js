df4679bc57f66a5c4169d5dd4dfe159c074a7a16
Victor Brambila <victorbrambila22@gmail.com>
2022-05-26 12:47:32 -0700

    store and issreducer set up

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './containers/App';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers/index.js';
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </BrowserRouter>
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux";
+import issReducer from "./issReducer";
+
+export default combineReducers({ issReducer });
