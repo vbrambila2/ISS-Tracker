7f64866684f54c85ecf504907e6d58533382ab87
Victor Brambila <victorbrambila22@gmail.com>
2022-06-02 14:56:35 -0700

    homepage css done

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -51,18 +51,18 @@ function HomePage(props) {
   }, [])
 
     return ( 
-        <div className={classes.Home}>
-            <h1 className={classes.appName}>International Space Station Tracker</h1>
-            <p className={classes.appDescription}>Follow the ISS on it's journey around the globe. 
-            Get real time data on it's location, visibility, current astronauts on board and more!
-            </p>
-            <p className={classes.instructions}>To begin, open the menu and select ISS Map</p>
-            <h2 className={classes.potd}>NASA Photo of the Day</h2>
-            <div className={classes.photoTitle}>Title: {photo.title}</div>
-            <div className={classes.photoEx}>Description: {photo.explanation}</div>
-            <div className={classes.photoDate}>Photo date: {photo.date}</div>
-            <NasaPhoto photo={photo.hdurl}/>
-        </div>
+      <div className={classes.Home}>
+        <h1 className={classes.appName}>International Space Station Tracker</h1>
+        <p className={classes.appDescription}>Follow the ISS on it's journey around the globe. 
+          Get real time data on it's location, visibility, current astronauts on board and more!
+        </p>
+        <p className={classes.instructions}>To begin, open the menu and select ISS Map</p>
+        <h2 className={classes.potd}>NASA Photo of the Day</h2>
+        <div className={classes.photoTitle}>Title: {photo.title}</div>
+        <div className={classes.photoEx}>Description: {photo.explanation}</div>
+        <div className={classes.photoDate}>Photo date: {photo.date}</div>
+        <NasaPhoto photo={photo.hdurl}/>
+      </div>
     ); 
 };
 
@@ -74,9 +74,9 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-      getPhoto: () => {
-          dispatch(getPhoto())
-      }
+    getPhoto: () => {
+      dispatch(getPhoto())
+    }
   }
 }
 
