708569c35526868d36acd8361228cd604c62cb9d
Victor Brambila <victorbrambila22@gmail.com>
2023-01-09 22:13:20 -0800

    initial paragraph added to about

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -21,6 +21,16 @@ function About(props) {
     return (
         <div className={classes.About}>
             <section className={classes.aboutTitle}>About the ISS</section>
+            <p>The space station is Earth's only microgravity laboratory. 
+                This football field-sized platform hosts a plethora of science and 
+                technology experiments that are continuously being conducted by 
+                crew members, or are automated. Research aboard the orbiting 
+                laboratory holds benefits for life back on Earth, as well as for 
+                future space exploration. The space station serves as a testbed for 
+                technologies and allows us to study the impacts of long-term 
+                spaceflight to humans, supporting NASA's mission to push human 
+                presence farther into space.
+            </p>
         </div>
     )
 }
