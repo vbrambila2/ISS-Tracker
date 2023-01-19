8a8cea2825ea9aac518d64ab71a762cbefa39519
Victor Brambila <victorbrambila22@gmail.com>
2023-01-18 21:27:31 -0800

    faq answers turned into const

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -54,6 +54,27 @@ function About(props) {
     const classes = useStyles(props);
     const { toggleFaq, isFaqOpen, faqId } = props;
 
+    const answerOne = `
+        The space station is Earth's only microgravity laboratory. This football field-sized 
+        platform hosts a plethora of science and technology experiments that are continuously 
+        being conducted by crew members, or are automated. Research aboard the orbiting 
+        laboratory holds benefits for life back on Earth, as well as for future space exploration. 
+        The space station serves as a testbed for technologies and allows us to study the 
+        impacts of long-term spaceflight to humans, supporting NASA's mission to push human 
+        presence farther into space. Learn more about the research happening on the space station,
+         and opportunities to conduct your science there
+    `
+    const answerTwo = `
+        The space station is visible because it reflects the light of the Sun – the same reason 
+        we can see the Moon. However, unlike the Moon, the space station isn't bright enough to 
+        see during the day. It can only be seen when it is dawn or dusk at your location. As such, 
+        it can range from one sighting opportunity a month to several a week, since it has to be 
+        both dark where you are, and the space station has to happen to be going overhead
+    `
+    const answerThree = `
+        No, you can see the space station with your bare eyes, no equipment required
+    `
+
     return (
         <div>
             <div className={classes.About}>
@@ -75,11 +96,11 @@ function About(props) {
                 <section>
                     <div className={classes.faqTitle}>Frequently asked questions</div>
                     <div className={classes.faqQuestion} onClick={() => toggleFaq(1)}>Why is the space station up there?</div>
-                    <p className={isFaqOpen && faqId===1 ? classes.faqQuestionAnswerActive : classes.faqQuestionAnswer} id="1" >answer</p>
+                    <p className={isFaqOpen && faqId===1 ? classes.faqQuestionAnswerActive : classes.faqQuestionAnswer} id="1" >{answerOne}</p>
                     <div className={classes.faqQuestion} onClick={() => toggleFaq(2)}>How often can I expect to see the space station?</div>
-                    <p className={isFaqOpen && faqId===2 ? classes.faqQuestionAnswerActive : classes.faqQuestionAnswer} id="2" >answer</p>
+                    <p className={isFaqOpen && faqId===2 ? classes.faqQuestionAnswerActive : classes.faqQuestionAnswer} id="2" >{answerTwo}</p>
                     <div className={classes.faqQuestion} onClick={() => toggleFaq(3)}>Do I need a telescope to see the space station?</div>
-                    <p className={isFaqOpen && faqId===3 ? classes.faqQuestionAnswerActive : classes.faqQuestionAnswer} id="3" >answer</p>
+                    <p className={isFaqOpen && faqId===3 ? classes.faqQuestionAnswerActive : classes.faqQuestionAnswer} id="3" >{answerThree}</p>
                 </section>
             </div>
         </div>
