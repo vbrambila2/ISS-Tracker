e64c1708ded004a32769c91b353e51e54a6d8c30
Victor Brambila <victorbrambila22@gmail.com>
2023-01-19 21:26:09 -0800

    footer initiale structure created

---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => {
+  return ({
+
+  });
+});
+
+function Footer(props) {
+  //const classes = useStyles(props);
+
+  return (
+    <div >
+        Footer
+    </div>
+  );
+}
+
+export default Footer;
