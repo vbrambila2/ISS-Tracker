e46c7fb58259bcccf5a4f03f296785e02c2c81eb
Victor Brambila <victorbrambila22@gmail.com>
2023-01-19 21:38:52 -0800

    added options to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,16 +3,24 @@ import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => {
   return ({
-
+    footer: {
+        background: '#d9d9d9',
+        marginTop: '10rem',
+        padding: '7rem'
+    }
   });
 });
 
 function Footer(props) {
-  //const classes = useStyles(props);
+  const classes = useStyles(props);
 
   return (
-    <div >
-        Footer
+    <div className={classes.footer}>
+        <div>Copyright</div>
+        <div>Home</div>
+        <div>ISS Map</div>
+        <div>About</div>
+        <div>API</div>
     </div>
   );
 }
